0c42bd71ce60449f25ec0efcd112762dbafc616c
cp16jb <34383107+cp16jb@users.noreply.github.com>
2018-01-09 12:39:08 -0500

    Update collection.php

--- collection.php
+++ collection.php
@@ -1,13 +1,18 @@
 #!/usr/bin/php-cgi
-<?php
-$db_host = 'localhost'; // Server Name
-$db_user = 'cp16jb'; // Username
-$db_pass = '6082846'; // Password
-$db_name = 'cp16jb'; // Database Name
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+<!--
+Name: Cole Perry & Heshan Modaragamage
+Date: January 9th, 2017
+Course: COSC 2P89
+Final Project "The Collector"
+-->
+
+<?php
+
+$conn = mysqli_connect('localhost', 'cp16jb', '6082846', 'cp16jb');
 if (!$conn) {
-    die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+    die ('Oops! There was a problem connecting to the SQL server. Here are
+          some details about the crash: ' . mysqli_connect_error());
 }
 
 $sql = 'SELECT * 
@@ -16,66 +21,89 @@ $sql = 'SELECT *
 $query = mysqli_query($conn, $sql);
 
 if (!$query) {
-    die ('SQL Error: ' . mysqli_error($conn));
+    die ('Oops! There was an unexpected runtime error. 
+          Here are the details: ' . mysqli_error($conn));
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="UTF-8">
-    <title>Collection | Gaming in the 90s Store</title>
+    <title>Collection | The 90s Collector</title>
 </head>
 <body>
 <header>
-    <img src="Images/sitelogo.PNG" href="home.html">
+    <a href="home.html"><img src="Images/sitelogo.PNG" alt="the 90s collector"></a>
     <br/>
 </header>
 <nav align="right">
     <a href="home.html">Home</a> |
-    <a href="collection.html">Collection</a> |
-    <a href="login.html">Login</a>
+    <a href="collection.php">Collection</a> |
+    <a href="login.html">Login</a> |
+    <a href="addgame.html">Add Game</a>
 </nav>
 <div align="center">
     <h2 align="center" class="collection_description">
-        Below is our growing collection of videogames to choose from! </br>
-        Please note that it's not possible to order directly from our site yet, but we're working on it!
+        Below is our growing collection of videogames to choose from!
     </h2>
+    <br/>
 
-    <table>
+    <table class="collection-table">
         <thead>
         <tr>
-            <th>TITLE</th>
-            <th>PUBLISHER</th>
-            <th>RELEASE DATE</th>
-            <th>CONSOLE</th>
-            <th>ESRB RATING</th>
-            <th>COPIES AVAILABLE</th>
+            <th>IMAGE</th>
+            <th>INFO</th>
         </tr>
         </thead>
         <tbody>
         <?php
         while ($row = mysqli_fetch_array($query))
         {
-            // $amount  = $row['amount'] == 0 ? '' : number_format($row['amount']);
-            echo '<tr>
-					<td>'.$row['title'].'</td>
-					<td>'.$row['publisher'].'</td>
-					<td>'.$row['release_date'].'</td>
-					<td>'.$row['console'].'</td>
-					<td>'.$row['esrb_rating'].'</td>
-					<td>'.$row['copies_available'].'</td>
+            if ($row['picture'] == NULL) {
+                $img = 'Images/default_image.png';
+            } else {
+                $img = $row['picture'];
+            }
+
+            if ($row['copies_available'] <= 0) {
+                $copies_available = "** SOLD OUT **";
+            } else {
+                $copies_available = $row['copies_available'];
+            }
+
+            $title = $row['title'];
+            $publisher = $row['publisher'];
+            $release_date = $row['release_date'];
+            $console = $row['console'];
+            $esrb_rating = $row['esrb_rating'];
+
+
+            echo '<tr class="collection-info">
+                    <td>
+                        <img src='.$img.' class="box-art" align="center"/>
+                    </td>
+					<td>
+					    Title: '.$row['title'].'<br/>
+					    Publisher: '.$row['publisher'].'<br/>
+					    Release Date: '.$row['release_date'].'<br/>
+					    Console: '.$row['console'].'<br/>
+					    ESRB: '.$row['esrb_rating'].'<br/>
+					    <strong>Copies Available: '.$copies_available.'</strong>
+					</td>
 				</tr>';
         }?>
         </tbody>
-        <tfoot>
-        <tr>
-            <th colspan="4">TOTAL</th>
-            <th><?=number_format($total)?></th>
-        </tr>
-        </tfoot>
     </table>
 </div>
+<footer>
+    <br/>
+    <p align="center">This site was created by Cole Perry and Heshan Modaragamage.</p>
+    <p align="center"><a href="reference.html">Site References</a></p>
+    <br/>
+    <br/>
+</footer>
 </body>
 </html>
